77a8de641e4176fcbc84fe0e7f2a1ee596ab7b7c
imAlessas <73311498+imAlessas@users.noreply.github.com>
2025-03-04 20:48:30 +0100

    Create +page.server.ts

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,12 @@
+import type { Actions } from './$types';
+
+export const actions: Actions = {
+
+    signIn: async ({ locals }) => {
+        await locals.logtoClient.signIn('http://localhost:3000/callback');
+    },
+
+    signOut: async ({ locals }) => {
+        await locals.logtoClient.signOut('http://localhost:3000/');
+    },
+};

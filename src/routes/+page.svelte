11e23c1e45e8838693e48e5e011fcdba4a69e43b
Alessandro Trigolo <alessandrotrigolo@gmail.com>
2025-05-19 22:54:32 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,16 @@
-<script lang='ts'>
+<script>
+    import { goto } from '$app/navigation';
+    import { onMount } from 'svelte';
 
-    
+    export let data;
+
+    onMount( () => {
+        if(data.user) goto("/list");
+    });
 
 </script>
 
-<!-- <form method="POST" action="?/{data.user ? 'signOut' : 'signIn'}">
+<form method="POST" action="?/{data.user ? 'signOut' : 'signIn'}">
     <button type="submit">Sign {data.user ? 'out' : 'in'}</button>
-</form> -->
+</form>
 

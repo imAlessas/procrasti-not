e0490baf4732038e710bbaaf9c52afd41b8c7984
Alessandro Trigolo <alessandrotrigolo@gmail.com>
2025-06-11 20:48:48 +0200

    Update database connection function

--- src/lib/server/mongoose.ts
+++ src/lib/server/mongoose.ts
@@ -1,15 +1,27 @@
 import mongoose from 'mongoose';
-import { env } from '$env/dynamic/private';
+import { MONGO_URL } from '$env/static/private';
 
+declare global {
+  var mongoose: {
+    conn: typeof mongoose | null;
+    promise: Promise<typeof mongoose> | null;
+  };
+}
 
-let isConnected = false;
+let cached = global.mongoose;
+if (!cached)
+	cached = global.mongoose = { conn: null, promise: null };
 
-export async function connectDB() {
-    if (isConnected)
-        return;
+export async function connectToMongoDB() {
 
-    await mongoose.connect(env.MONGO_URL!);
+	if (cached.conn)
+        return cached.conn;
 
-    isConnected = true;
-    console.log('MongoDB connected');
+	if (!cached.promise) {
+		cached.promise = mongoose.connect(MONGO_URL)
+			.then( (mongoose) => { return mongoose;} );
+	}
+
+	cached.conn = await cached.promise;
+	return cached.conn;
 }

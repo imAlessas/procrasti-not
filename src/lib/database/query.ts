1f0e0cffba1719ba298de1209bf920cc59fa0787
Alessandro Trigolo <alessandrotrigolo@gmail.com>
2025-06-12 22:20:46 +0200

    Create query.ts

---
+++ src/lib/database/query.ts
@@ -0,0 +1,34 @@
+import { Users } from "$lib/models/Users";
+import { connectToMongoDB } from "./mongoose";
+
+
+function serialize(doc: any) {
+    return {...doc};
+}
+
+
+
+
+export const findUser = async (id : string) : Promise<DatabaseUser | null> => {
+    await connectToMongoDB();
+
+    const constraints = { userId: id  }
+    const user = await Users.findOne(constraints).lean<DatabaseUser | null>();
+
+    if (!user) return null;
+
+    return {
+        userId:     user.userId,
+        username:   user.username,
+        email:      user.email,
+        created:    user.created
+    }
+};
+
+
+export const createUser = async (newUser : DatabaseUser) : Promise<void> => {
+    await connectToMongoDB();
+
+    await Users.create(newUser);
+
+};

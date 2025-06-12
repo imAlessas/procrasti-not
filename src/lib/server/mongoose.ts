a6d8910f331396099b274f3309c45dedb8249c19
Alessandro Trigolo <alessandrotrigolo@gmail.com>
2025-06-12 20:47:30 +0200

    Update mongoose.ts

--- src/lib/server/mongoose.ts
+++ src/lib/server/mongoose.ts
@@ -1,27 +1,34 @@
 import mongoose from 'mongoose';
 import { MONGO_URL } from '$env/static/private';
 
+// Create the global variable mongoose
 declare global {
   var mongoose: {
-    conn: typeof mongoose | null;
-    promise: Promise<typeof mongoose> | null;
+    conn: typeof import("mongoose") | null;                         // Active mongoose connection, if any
+    promise: Promise<typeof import("mongoose")> | null;             // Promise of onging connection, if any
   };
 }
 
+
+// Retrieve the cached variable
 let cached = global.mongoose;
-if (!cached)
-	cached = global.mongoose = { conn: null, promise: null };
+if (!cached)    // If none, initialize it
+	cached = global.mongoose = {
+        conn: null,
+        promise: null
+    };
+
 
 export async function connectToMongoDB() {
 
 	if (cached.conn)
         return cached.conn;
 
-	if (!cached.promise) {
-		cached.promise = mongoose.connect(MONGO_URL)
-			.then( (mongoose) => { return mongoose;} );
-	}
+    // If there is no connection attempt, start one
+	if (!cached.promise)
+		cached.promise = mongoose.connect(MONGO_URL).then((mongoose) => {return mongoose;});
 
+    // Cache the connection and return
 	cached.conn = await cached.promise;
 	return cached.conn;
 }

090901750911355c84c0c032c1f9038b0aee5c56
Alessandro Trigolo <alessandrotrigolo@gmail.com>
2025-06-12 22:20:58 +0200

    Update users.ts

--- src/lib/models/users.ts
+++ src/lib/models/users.ts
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose';
 
 const usersSchema = new mongoose.Schema({
-    name: String,
-    email: String
+    userId: String,
+    username: String,
+    email: String,
+    created: Number
 });
 
 export const Users = mongoose.models.Users || mongoose.model('Users', usersSchema);

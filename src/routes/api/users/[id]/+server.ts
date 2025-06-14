283931ef6820061aab016a386f2d4dab7a1aafef
Alessandro Trigolo <alessandrotrigolo@gmail.com>
2025-06-14 12:52:34 +0200

    Refactor

---
+++ src/routes/api/users/[id]/+server.ts
@@ -0,0 +1,26 @@
+// src/routes/api/users/[id].ts
+import type { RequestHandler } from '@sveltejs/kit';
+import { createUser, findUser } from '$lib/database/query';
+
+
+export const GET: RequestHandler = async ({ params }) => {
+
+    if (!params.id)
+        return new Response(JSON.stringify({error: 'Property [id] is mandatory.'}), { status: 200 });
+
+    const user = await findUser(params.id);
+    if (!user)
+        return new Response(JSON.stringify({error: 'User not found.'}), { status: 200 });
+
+    return new Response(JSON.stringify(user), { status: 200 });
+};
+
+
+export const POST: RequestHandler = async ({ request }) => {
+
+    const data = await request.json();
+    await createUser(data);
+
+    return new Response(JSON.stringify({}), { status: 200 });
+
+};

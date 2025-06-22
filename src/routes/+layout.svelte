9ac0fc9b6f889eab9807b0119b371086004a8bb0
Alessandro Trigolo <alessandrotrigolo@gmail.com>
2025-06-22 16:57:39 +0200

    Create +layout.svelte
    Manages the themes

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script>
+    import '$lib/styles/style.scss';
+    import { theme } from '$lib/store/theme';
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        document.body.classList.remove('light', 'dark');
+        document.body.classList.add($theme);
+    });
+
+    function handleChange() {
+        document.body.classList.remove('light', 'dark');
+        document.body.classList.add($theme)
+    }
+
+</script>
+
+<select id="theme-select" bind:value={$theme} on:change={handleChange}>
+    <option value="light">Light</option>
+    <option value="dark">Dark</option>
+</select>
+
+<slot />

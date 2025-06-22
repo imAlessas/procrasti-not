cd194c0a2b15cd727ece0f7fb0a938000a757c36
Alessandro Trigolo <alessandrotrigolo@gmail.com>
2025-06-22 17:38:31 +0200

    Create Button components

---
+++ src/components/SecondaryButton.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  export let onClick: () => any = () => {};
+</script>
+
+<button class="secondary-button non-selectable" onclick={ onClick }>
+    <span class="not-cover">
+        <slot />
+    </span>
+</button>

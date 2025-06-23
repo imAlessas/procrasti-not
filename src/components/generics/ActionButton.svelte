08bd3b2a0071db2a0c3a2fd8954e1b3b80dd5a84
Alessandro Trigolo <alessandrotrigolo@gmail.com>
2025-06-23 21:51:04 +0200

    Create ActionButton.svelte

---
+++ src/components/generics/ActionButton.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import Icon from "./Icon.svelte";
+    
+    export let icon;
+    export let onClick: () => any = () => {};
+    export let type : "complete" | "delete";
+
+</script>
+
+
+<button class="action-button {type} non-selectable " onclick={onClick}>
+    <Icon {icon} />
+</button>

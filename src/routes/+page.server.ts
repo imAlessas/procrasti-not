aca484cb3d72c435754d84811da55c66f54ed0bc
Alessandro Trigolo <alessandrotrigolo@gmail.com>
2025-06-29 17:34:45 +0200

    Update +page.server.ts

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,14 +1,9 @@
 import { BASE_URL } from '$env/static/private';
-import { LOGGED_USER_SESSION } from '$lib/utils/const';
 import type { Actions } from './$types';
 
 
 export const actions: Actions = {
     signIn: async ({ locals }) => {
         await locals.logtoClient.signIn(`${BASE_URL}callback`);
-    },
-    signOut: async ({ locals }) => {
-        sessionStorage.removeItem(LOGGED_USER_SESSION);     // Remove logged user from session
-        await locals.logtoClient.signOut((`${BASE_URL}`));
-    },
+    }
 };
